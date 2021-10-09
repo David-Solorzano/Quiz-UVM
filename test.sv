c2d15a35caa3c1de61e74620b27249c9524395c9
David Solorzano <david.jose.0503@gmail.com>
2021-10-09 00:13:48 -0600

    Add files via upload

--- test.sv
+++ test.sv
@@ -20,13 +20,13 @@ class test extends uvm_test;
     endfunction
 
     virtual task run_phase(uvm_phase phase);
+        random_item_sequence random_seq = random_item_sequence::type_id::create("random_seq");
         phase.raise_objection(this);
-
         // Reset
         apply_reset();
         #50;
         // Secuencia aleatoria
-        random_item_sequence random_seq = random_item_sequence::type_id::create("random_seq");
+
         random_seq.randomize();
         random_seq.start(e0.a0.sequencer_inst)
         #50;
