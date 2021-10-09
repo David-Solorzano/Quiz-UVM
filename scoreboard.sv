7d4de965fda2628472f0aa7184c54826e0afeaca
David Solorzano <david.jose.0503@gmail.com>
2021-10-09 01:19:05 -0600

    Add files via upload

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,7 +1,7 @@
 import uvm_pkg::*;
 
 class scoreboard extends uvm_scoreboard;
-    `uvm_object_utils(scoreboard)
+    `uvm_component_utils(scoreboard)
 
     function new(string name = "scoreboard", uvm_component parent = null);
         super.new(name, parent);
